c160ee0f88a7923344544fbf6680f5cae4b9324b
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-08-25 12:59:53 +0600

    Update test_parpas.cpp

--- URI/BEGINNER/test_parpas.cpp
+++ URI/BEGINNER/test_parpas.cpp
@@ -3,6 +3,13 @@ using namespace std;
 int main()
 {
     cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+    cout<<"Pulock"<<endl;
+
 
     return 0;
 }
