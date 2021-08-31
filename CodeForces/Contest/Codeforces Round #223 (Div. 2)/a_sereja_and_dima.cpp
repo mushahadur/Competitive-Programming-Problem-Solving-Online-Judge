b55f7f39e4dc232bcdf04796a9fa45941cb48835
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-08-31 14:27:24 +0600

    Update a_sereja_and_dima.cpp

--- CodeForces/Contest/Codeforces Round #223 (Div. 2)/a_sereja_and_dima.cpp
+++ CodeForces/Contest/Codeforces Round #223 (Div. 2)/a_sereja_and_dima.cpp
@@ -1,3 +1,10 @@
+//! Game - Over.
+/** ========================================**
+ ** @Author:   Md. Mushahadur Rahaman Khan
+               ( City University of Bangladesh, CSE 49th )
+ ** @Category: The Standard Template Library.
+/** ========================================**/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
