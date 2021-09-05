39927564fdad2a9025e8880fb59e0ef4d65f7f7a
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-05 18:11:39 +0600

    Create shuffling_parities.cpp

---
+++ CodeChef/Contest/Long Contest/Scorable Problems for Division 3/shuffling_parities.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+using namespace std;
+int main()
+{
+    optimize();
+
+    map<int, int>v;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+    }
+
+    return 0;
+}
+
