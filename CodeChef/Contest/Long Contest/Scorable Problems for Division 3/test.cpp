8dc3fc6d4a00c3c2dcf4d6b7fa58b1a84215ca4f
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-06 13:22:22 +0600

    Update test.cpp

--- CodeChef/Contest/Long Contest/Scorable Problems for Division 3/test.cpp
+++ CodeChef/Contest/Long Contest/Scorable Problems for Division 3/test.cpp
@@ -3,33 +3,11 @@ using namespace std;
 int main()
 {
 
-    int t,ans;
-    cin>>t;
-    cout<<t<<endl;
-    while(t--)
-    {
-        int n,a,b;
-        cin>>n>>a>>b;
-        cout<<n<<" "<<a<<" "<<b<<" "<<endl;
+for(int j=0; j<2; j++){
+    cout<<j<<endl;
+}
 
-            ans=0;
-            string s;
-            cin>>s;
 
-            cout<<"String : "<<s<<endl;
-            for(auto i:s)
-            {
-                char c = i;
-                cout<<"char : "<<c<<endl;
-                if(c=='0')
-                {
-                    ans+=a;
 
-                }
-                else ans+=b;
-            }
-
-        cout<<"ans  : "<<ans<<endl;
-    }
     return 0;
 }
