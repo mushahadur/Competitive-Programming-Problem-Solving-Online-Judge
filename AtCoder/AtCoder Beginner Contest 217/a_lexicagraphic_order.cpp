83dd0aa0010a09d115f91d79d054351d9d189d7d
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-11 06:27:12 +0600

    Create a_lexicagraphic_order.cpp

---
+++ AtCoder/AtCoder Beginner Contest 217/a_lexicagraphic_order.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+using namespace std;
+int main()
+{
+    optimize();
+    string s, t;
+    cin>>s>>t;
+    if(s<t)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+
+    return 0;
+}
