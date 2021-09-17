5422aa4249cf9f29516afa0dcc3268026e5d3d4f
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-09-17 08:49:08 +0600

    Create is_it_a_vowel_or_consonent.cpp

---
+++ CodeChef/Begenner/is_it_a_vowel_or_consonent.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+using namespace std;
+int main()
+{
+    optimize();
+        string c;
+        cin>>c;
+            if((c=='A' || c=='E' || c=='I' || c=='O' || c=='U')){
+                cout<<"vol"<<endl;
+            }
+        cout<<"con"<<endl;
+    return 0;
+}
