809df4a2f75f0e6f20f838646f317c2f2041d0b8
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-11-22 15:15:36 +0600

    Create a-exact_price.cpp

---
+++ AtCoder/AtCoder Beginner Contest 223/a-exact_price.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    if(n%100==0){
+        cout<<"Yes"<<endl;
+    }
+    else cout<<"Yes"<<endl;
+
+    return 0;
+    }
