dbc0ec654eb7654bb4dff4888178cbeb2e6c690f
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2021-11-22 15:15:53 +0600

    Create love_calculator_test.cpp

---
+++ UHunt/Introduction/The Easy Problems/Function/love_calculator_test.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ char a[111],b[111];
+ while(scanf("%[^\n]s",&a)!=EOF)
+ {
+     scanf("%*c");
+     scanf("%[^\n]s",&b);
+
+     cout<<a<<endl;
+     cout<<b<<endl;
+
+     scanf("%*c");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
