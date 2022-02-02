1d10f33a3c86bc3e7595b8dafd549350ee6e4cc6
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2022-02-02 12:18:15 +0600

    Create a_counting.cpp

---
+++ AtCoder/AtCoder Beginner Contest 209/a_counting.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cin>>num1>>num2;
+    if(num1>num2) cout<<0<<endl;
+
+    else printf("%d\n",(num2-num1)+1);
+
+    return 0;
+}
