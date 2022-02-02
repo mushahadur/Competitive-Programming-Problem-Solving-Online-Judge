021ab51854463a2c237bf6607ec0570818c7627a
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2022-02-02 12:19:07 +0600

    Create test.cpp

---
+++ CodeForces/Contest/Codeforces Beta Round #55 (Div. 2)/test.cpp
@@ -0,0 +1,37 @@
+#include <cctype>
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    while(1){
+    int w, i, j, c_u=0,c_l=0;
+    string str;
+    cin>>str;
+    w = str.size();
+    for(i=0; i<w; i++){
+        if(str[i]>96){
+           c_l++;
+        }
+        else{
+           c_u++;
+        }
+    }
+
+    for(j=0; j<w; j++){
+        if(c_l >= c_u){
+            str[j]=tolower(str[j]);
+
+        }
+        else{
+
+           str[j]=toupper(str[j]);
+        }
+    }
+
+    cout << str<<endl;
+    }
+
+
+    return 0;
+}
