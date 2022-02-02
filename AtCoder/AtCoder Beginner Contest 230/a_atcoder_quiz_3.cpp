091d59c23a8174cbd4ed6fea2f1f067e3ac1e205
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2022-02-02 12:18:24 +0600

    Create a_atcoder_quiz_3.cpp

---
+++ AtCoder/AtCoder Beginner Contest 230/a_atcoder_quiz_3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    if(num<55){
+        if(num>41) printf("AGC%03d\n",num+1);
+
+        else printf("AGC%03d\n",num);
+    }
+
+    return 0;
+}
