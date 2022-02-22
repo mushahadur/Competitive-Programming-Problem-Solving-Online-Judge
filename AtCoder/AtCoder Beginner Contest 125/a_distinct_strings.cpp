98b6a664e5810d6907d120d002a7cddf99f1708a
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2022-02-22 18:37:29 +0600

    Create a_distinct_strings.cpp

---
+++ AtCoder/AtCoder Beginner Contest 125/a_distinct_strings.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+    if(str[0] != str[1] && str[1] != str[2] && str[0] != str[2]){
+        cout<<6<<endl;
+    }
+    else if(str[0]==str[1] && str[0]==str[2]){
+        cout<<1<<endl;
+    }
+    else{
+        cout<<3<<endl;
+    }
+
+    return 0;
+}
